042c8a0a6c928549314b35d21843672343ab2ccd
Emily H. Zhou <f003wqk@plank.thayer.dartmouth.edu>
2021-08-25 13:01:33 -0400

    8/25 work

---
+++ sudoku.c
@@ -0,0 +1,42 @@
+/**
+ * sudoku.c - contains functions that help create sudoku puzzles
+ * 
+ * CS50, Summer 2021
+ * Authors: Clay Papanek, Emily Zhou, Melissa Valencia, and Noah Yusen
+ */
+
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "sudokulib.h"
+#include "solver.h"
+#include "create.h"
+
+
+int main(int argc, char const *argv[]) {
+    if(argc != 2){
+		fprintf(stderr, "usage: ./sudoku (solve/create)\n");
+		exit(1);
+	}
+
+    if (strcmp(argv[1], "create") == 0) {
+        puzzle_t *puzzle = create();
+        puzzle_print(puzzle, stdout);
+        puzzle_delete(puzzle);
+    } else if (strcmp(argv[1], "solve") == 0) {
+        puzzle_t *puzzle = puzzle_load(stdin);
+        if (puzzle == NULL || !puzzle_solve_forwards(puzzle)) {
+            fprintf(stderr, "puzzle is unsolvable" );
+            exit(2);
+        } 
+        puzzle_solve_forwards(puzzle);
+        puzzle_print(puzzle, stdout);
+        puzzle_delete(puzzle);
+    } else {
+        fprintf(stderr, "usage: ./sudoku (solve/create)\n");
+		exit(3);
+    }
+    return 0;
+}
